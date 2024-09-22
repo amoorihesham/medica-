2962271a7c330041a9da8e2b64dc9fe30fe3af0f
AmrHesham <msam.team20@gmail.com>
2024-09-22 17:45:11 +0300

    Solved Error On server

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,3 +1,5 @@
 const Cart = () => {
   return <h1>Cart Pgae</h1>;
 };
+
+export default Cart;
--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,3 +1,5 @@
 const Checkout = () => {
   return <h1>Checkout Pgae</h1>;
 };
+
+export default Checkout;
